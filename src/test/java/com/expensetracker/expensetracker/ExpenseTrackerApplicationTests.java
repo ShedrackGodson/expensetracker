ad9e6dcbe58a171692a75ccfa5ae1ad1321e5a3d
ShedrackGodson <hedrickgodson23@gmail.com>
2021-09-30 16:49:30 +0300

    Initial commit

---
+++ src/test/java/com/expensetracker/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetracker.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
